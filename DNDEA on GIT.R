5b4b65c7f0089affaaba68a87c018440f6025196
tgno3 <tgno3.com@gmail.com>
2021-04-20 22:25:08 +0900

    More discussions added

--- DNDEA on GIT.R
+++ DNDEA on GIT.R
@@ -71,80 +71,66 @@ table.5 <- data.frame(Name          = rownames(df.3d),
                       S2.overall.BS = apply(res.dndea.bs$eff.s2, 1, "mean"),
                       S2.2016.BS    = res.dndea.bs$eff.s2[,,3])
 
+# Figure TBD
+id.ex <- c(8, 14, 19, 20)
+df.gg <- data.frame(Name     = rep(rownames(df.3d)[id.ex], each = 4),
+                    Model    = rep(c("NS", "SS", "FS", "BS"), each = 4),
+                    Eff.2014 = res.dndea.ns$eff.s2[id.ex,,1],
+                    Eff.2015 = res.dndea.ns$eff.s2[id.ex,,2],
+                    Eff.2016 = res.dndea.ns$eff.s2[id.ex,,3],
+                    Eff.2017 = res.dndea.ns$eff.s2[id.ex,,4],
+                    Eff.2018 = res.dndea.ns$eff.s2[id.ex,,5])
 
-# Discussions
+# KRICT
+id.ex <- c(19)
+beta  <- round(c(res.dndea.ss$beta [id.ex,,2], res.dndea.fs$beta [id.ex,,2], res.dndea.bs$beta [id.ex,,2]), 2)
+gamma <- round(c(res.dndea.ss$gamma[id.ex,,1], res.dndea.fs$gamma[id.ex,,1], res.dndea.bs$gamma[id.ex,,1]), 2)
+beta + gamma
+
+
 # KIST and KRISS: BS.2016 < SS.2016 but BS.All > SS.All
 id.ex <- c(14, 20)
-data.frame(Eff.2016.SS = round(res.dndea.ss$eff.s2[id.ex,,3], 4),
+data.frame(Name = table.5$Name[id.ex],
+           Eff.2016.SS = round(res.dndea.ss$eff.s2[id.ex,,3], 4),
            Eff.2016.BS = round(res.dndea.bs$eff.s2[id.ex,,3], 4),
            Eff.All.SS  = round(apply(res.dndea.ss$eff.s2[id.ex,,], 1, "mean"), 4),
            Eff.All.BS  = round(apply(res.dndea.bs$eff.s2[id.ex,,], 1, "mean"), 4))
 
 
 # KICT: FS.2016 > BS.2016 > NS.2016 > SS.2016
-id.ex      <- c(8)
-data.frame(Eff.2016.NS = round(res.dndea.ss$eff.s2[id.ex,,3], 4),
-           Eff.2016.FS = round(res.dndea.fs$eff.s2[id.ex,,3], 4),
-           Eff.2016.BS = round(res.dndea.bs$eff.s2[id.ex,,3], 4))
-res.fs.pro <- data.frame(Alpha = res.dndea.fs$alpha[id.ex,,1:3],
-                         Beta  = res.dndea.fs$beta [id.ex,,1:3],
-                         Gamma = res.dndea.fs$gamma[id.ex,,1:3])
-res.bs.pro <- data.frame(Alpha = res.dndea.bs$alpha[id.ex,,1:3],
-                         Beta  = res.dndea.bs$beta [id.ex,,1:3],
-                         Gamma = res.dndea.bs$gamma[id.ex,,1:3])
-data.frame(Patent.actual = df.3d[id.ex, id.z, 3],
-           Patent.acc.ss = df.3d[id.ex, id.z, 1:3] %*% c(0.2,0.3,0.5),
-           Patent.acc.fs = df.3d[id.ex, id.z, 1:3] %*% c(res.fs.pro[1, 3], res.fs.pro[2, 2], res.fs.pro[3, 1]),
-           Patent.acc.bs = df.3d[id.ex, id.z, 1:3] %*% c(res.bs.pro[1, 3], res.bs.pro[2, 2], res.bs.pro[3, 1]))
+id.ex <- c(8)
+res.fs.pro <- data.frame(Alpha = res.dndea.fs$alpha[id.ex,,3],
+                         Beta  = res.dndea.fs$beta [id.ex,,2],
+                         Gamma = res.dndea.fs$gamma[id.ex,,1])
+res.bs.pro <- data.frame(Alpha = res.dndea.bs$alpha[id.ex,,3],
+                         Beta  = res.dndea.bs$beta [id.ex,,2],
+                         Gamma = res.dndea.bs$gamma[id.ex,,1])
+data.frame(Name = table.5$Name[id.ex],
+           Patent.actual = df.3d[id.ex, id.z, 3],
+           Patent.effective.ss = sum(df.3d[id.ex, id.z, 1:3] * c(0.2,0.3,0.5)),
+           Patent.effective.fs = sum(df.3d[id.ex, id.z, 1:3] * res.fs.pro),
+           Patent.effective.bs = sum(df.3d[id.ex, id.z, 1:3] * res.bs.pro))
 
 
-data.frame(Eff.2016.NS = round(table.5$S2.2016.NS, 4),
-           Eff.2016.SS = round(table.5$S2.2016.SS, 4),
-           Win         = round(table.5$S2.2016.SS - table.5$S2.2016.NS, 4))
-
-data.frame(Eff.2016.NS = round(table.5$S2.2016.NS, 4),
-           Eff.2016.FS = round(table.5$S2.2016.FS, 4),
-           Win         = round(table.5$S2.2016.FS - table.5$S2.2016.NS, 4))
-
-data.frame(Eff.2016.NS = round(table.5$S2.2016.NS, 4),
-           Eff.2016.BS = round(table.5$S2.2016.BS, 4),
-           Win         = round(table.5$S2.2016.BS - table.5$S2.2016.NS, 4))
-
-data.frame(Eff.2016.FS = round(table.5$S2.2016.FS, 4),
-           Eff.2016.BS = round(table.5$S2.2016.BS, 4),
-           Win         = round(table.5$S2.2016.FS - table.5$S2.2016.BS, 4))
-
-
+# KIT and KRIBB: no change
+id.ex <- c(2, 15, 16, 18)
+data.frame(Name = table.5$Name[id.ex],
+           W.NS = res.dndea.ns$w.s1[id.ex,,3],
+           W.SS = res.dndea.ss$w.s1[id.ex,,3],
+           W.FS = res.dndea.fs$w.s1[id.ex,,3],
+           W.BS = res.dndea.bs$w.s1[id.ex,,3])
 
 
 # Further investigation
 p.period <- 3
-data.frame(No.split      = res.dndea.ns$eff.s2[,,p.period],
-           Static.split  = res.dndea.ss$eff.s2[,,p.period],
+data.frame(Name          = rownames(df.3d),
+           No.split      = res.dndea.ns$eff.s2[,,p.period],
+           Fixed.split   = res.dndea.ss$eff.s2[,,p.period],
            Free.Split    = res.dndea.fs$eff.s2[,,p.period],
            Bounded.split = res.dndea.bs$eff.s2[,,p.period],
            NS.vs.BS      = ifelse(res.dndea.ns$eff.s2[,,p.period] <= res.dndea.bs$eff.s2[,,p.period], 1, 0),
-           Diff.NS.BS    = round(res.dndea.bs$eff.s2[,,p.period] - res.dndea.ns$eff.s2[,,p.period], 2),
-           Diff.SS.BS    = round(res.dndea.bs$eff.s2[,,p.period] - res.dndea.ss$eff.s2[,,p.period], 2),
-           Diff.FS.BS    = round(res.dndea.bs$eff.s2[,,p.period] - res.dndea.fs$eff.s2[,,p.period], 2))
+           Diff.NS.SS    = round(res.dndea.ss$eff.s2[,,p.period] - res.dndea.ns$eff.s2[,,p.period], 4),
+           Diff.NS.BS    = round(res.dndea.bs$eff.s2[,,p.period] - res.dndea.ns$eff.s2[,,p.period], 4),
+           Diff.SS.BS    = round(res.dndea.bs$eff.s2[,,p.period] - res.dndea.ss$eff.s2[,,p.period], 4),
+           Diff.FS.BS    = round(res.dndea.bs$eff.s2[,,p.period] - res.dndea.fs$eff.s2[,,p.period], 4))
 
-data.frame(Alpha = res.dndea.fs$alpha[,,1:p.period],
-           Beta  = res.dndea.fs$beta[,,1:p.period],
-           Gamma = res.dndea.fs$gamma[,,1:p.period])
-
-data.frame(Alpha = res.dndea.bs$alpha[,,1:p.period],
-           Beta  = res.dndea.bs$beta[,,1:p.period],
-           Gamma = res.dndea.bs$gamma[,,1:p.period])
-
-data.frame(Alpha = res.dndea.ss$alpha[,,1:p.period],
-           Beta  = res.dndea.ss$beta[,,1:p.period],
-           Gamma = res.dndea.ss$gamma[,,1:p.period])
-
-
-###############
-# Discussions #
-###############
-
-# DMU 19: (0,0.44,0) FS & (0.15, 0.36, 0.2) BS & (0.2, 0.3, 0.5) SS > NS - How time lag consideration changes the results
-
-# DMU 16, 18: No differences - zero weight attached to z
