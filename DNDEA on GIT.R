9c6df257fdfce5a9d9ae516426b59eaad3934305
tgno3 <tgno3.com@gmail.com>
2021-04-28 22:52:13 +0900

    Figure 4 added

--- DNDEA on GIT.R
+++ DNDEA on GIT.R
@@ -1,7 +1,7 @@
 #########################################################################################################################
-### Project  : Dynamic Network DEA with distributable carryovers
+### Project  : Dynamic Network DEA
 ### Script   : DNDEA on GIT.R
-### Contents : Non-cooperative model with dynamic carryovers
+### Contents : Measuring dynamic efficiency with variable time lag effects
 #########################################################################################################################
 
 #########################################################################################################################
@@ -9,7 +9,7 @@
 #########################################################################################################################
 
 # Load library
-pkgs <- c("DJL")
+pkgs <- c("DJL", "ggplot2", "showtext")
 sapply(pkgs, require, character.only = T)
 
 # Load data & parameters
@@ -47,15 +47,16 @@ table.4 <- data.frame(Min  = apply(df.3d, 2, "min"),
 
 
 # Table 5. Comparative results
+# No-split scenario
 res.dndea.ns <- dm.dynamic.network(df.3d[,id.x.s1,], df.3d[,id.y.s1,], df.3d[,id.z,], 
                                    df.3d[,id.x.s2,], df.3d[,id.y.s2,], rts)
-
+# Fixed-split scenario
 res.dndea.ss <- dm.dynamic.network(df.3d[,id.x.s1,], df.3d[,id.y.s1,], df.3d[,id.z,], 
                                    df.3d[,id.x.s2,], df.3d[,id.y.s2,], rts, alpha = c(0.5, 0.3, 0.2))
-
+# Free-split scenario
 res.dndea.fs <- dm.dynamic.network(df.3d[,id.x.s1,], df.3d[,id.y.s1,], df.3d[,id.z,],
                                    df.3d[,id.x.s2,], df.3d[,id.y.s2,], rts, alpha = "free", max.cp = 2)
-
+# Bounded-split scenario
 res.dndea.bs <- dm.dynamic.network(df.3d[,id.x.s1,], df.3d[,id.y.s1,], df.3d[,id.z,], 
                                    df.3d[,id.x.s2,], df.3d[,id.y.s2,], rts, alpha = "free", max.cp = 2, LB = c(0.2, 0.2, 0.1))
 
@@ -71,15 +72,6 @@ table.5 <- data.frame(Name          = rownames(df.3d),
                       S2.overall.BS = apply(res.dndea.bs$eff.s2, 1, "mean"),
                       S2.2016.BS    = res.dndea.bs$eff.s2[,,3])
 
-# Figure TBD
-id.ex <- c(8, 14, 19, 20)
-df.gg <- data.frame(Name     = rep(rownames(df.3d)[id.ex], each = 4),
-                    Model    = rep(c("NS", "SS", "FS", "BS"), each = 4),
-                    Eff.2014 = res.dndea.ns$eff.s2[id.ex,,1],
-                    Eff.2015 = res.dndea.ns$eff.s2[id.ex,,2],
-                    Eff.2016 = res.dndea.ns$eff.s2[id.ex,,3],
-                    Eff.2017 = res.dndea.ns$eff.s2[id.ex,,4],
-                    Eff.2018 = res.dndea.ns$eff.s2[id.ex,,5])
 
 # KRICT
 id.ex <- c(19)
@@ -112,7 +104,7 @@ data.frame(Name = table.5$Name[id.ex],
            Patent.effective.bs = sum(df.3d[id.ex, id.z, 1:3] * res.bs.pro))
 
 
-# KIT and KRIBB: no change
+# KAERI, KISTI, KIT and KRIBB: no change
 id.ex <- c(2, 15, 16, 18)
 data.frame(Name = table.5$Name[id.ex],
            W.NS = res.dndea.ns$w.s1[id.ex,,3],
@@ -121,16 +113,50 @@ data.frame(Name = table.5$Name[id.ex],
            W.BS = res.dndea.bs$w.s1[id.ex,,3])
 
 
-# Further investigation
-p.period <- 3
-data.frame(Name          = rownames(df.3d),
-           No.split      = res.dndea.ns$eff.s2[,,p.period],
-           Fixed.split   = res.dndea.ss$eff.s2[,,p.period],
-           Free.Split    = res.dndea.fs$eff.s2[,,p.period],
-           Bounded.split = res.dndea.bs$eff.s2[,,p.period],
-           NS.vs.BS      = ifelse(res.dndea.ns$eff.s2[,,p.period] <= res.dndea.bs$eff.s2[,,p.period], 1, 0),
-           Diff.NS.SS    = round(res.dndea.ss$eff.s2[,,p.period] - res.dndea.ns$eff.s2[,,p.period], 4),
-           Diff.NS.BS    = round(res.dndea.bs$eff.s2[,,p.period] - res.dndea.ns$eff.s2[,,p.period], 4),
-           Diff.SS.BS    = round(res.dndea.bs$eff.s2[,,p.period] - res.dndea.ss$eff.s2[,,p.period], 4),
-           Diff.FS.BS    = round(res.dndea.bs$eff.s2[,,p.period] - res.dndea.fs$eff.s2[,,p.period], 4))
+# Figure 4. Efficiency changes
+id.ex     <- c(3, 5, 8, 19, 20, 22)
+id.p      <- 4
+{
+  id.dmu    <- id.ex[id.p]
+  scenario  <- c("No-split", "Fixed-split", "Free-split", "Bounded-split")
+  df.p.per  <- df.3d[,id.z,] / rowSums(df.3d[,id.z,]) * 100
+  df.r.per  <- df.3d[,id.y.s2,] / rowSums(df.3d[,id.y.s2,]) * 100
+  df.eff.s2 <- array(c(res.dndea.ns$eff.s2, res.dndea.ss$eff.s2, res.dndea.fs$eff.s2, res.dndea.bs$eff.s2), 
+                     c(nrow(df.3d), 5, 4), dimnames = list(rownames(df.3d), 2014:2018, scenario))
+  df.y1.ex  <- data.frame(Year       = rep(2014:2018, 4),
+                          Scenario   = rep(scenario, each = 5),
+                          Efficiency = c((df.eff.s2[id.dmu,,])))
+  df.y2.ex  <- data.frame(Year       = rep(2014:2018, 2),
+                          Type       = rep(c("Patent", "Royalty"), each = 5),
+                          Proportion = c(df.p.per[id.dmu,], df.r.per[id.dmu,]))
+  df.scale  <- data.frame(DMU = id.ex,
+                          MUL = c(120 ,100, 75, 75, 100, 100),
+                          MIS = c(36, 40, 45, 45, 30, 50))
+  font_add('Cambria', 'Cambria.ttf'); showtext_auto()
+  ggplot(df.y2.ex) +
+    geom_col (df.y2.ex, mapping = aes(x = Year, y = Proportion, fill = Type), position = 'dodge', width = 0.5) +
+    geom_line(df.y1.ex, mapping = aes(x = Year, y = Efficiency * df.scale[id.p, 2] - df.scale[id.p, 3], 
+                                      color = Scenario), size = 1.6) +
+    scale_y_continuous(limits   = c(0, 30), breaks = seq(0, 30, 10), 
+                       labels   = paste0(seq(0, 30, 10), "%"), guide = guide_axis(position = 'right'), 
+                       sec.axis = sec_axis(~./df.scale[id.p, 2] + df.scale[id.p, 3]/df.scale[id.p, 2], 
+                                           guide = guide_axis(position = 'left'))) + 
+    scale_fill_manual (values = c("Patent" = "#e2f0d9", "Royalty" = "#fbe5d6")) +
+    scale_color_manual(values = c("Bounded-split" = "#0070c0", "Fixed-split" = "#7c7c7c", "Free-split" = "#f9312c", "No-split" = "#0c0b0a")) +
+    ggtitle(rownames(df.3d)[id.dmu]) +
+    theme(legend.position = "bottom", 
+          legend.background = element_rect(fill = "white"),
+          legend.key = element_rect(fill = "white"),
+          legend.title = element_blank(),
+          legend.text = element_text(family = "Cambria"),
+          axis.ticks = element_blank(),
+          axis.text = element_text(family = "Cambria", size = 20),
+          axis.title.y.left = element_blank(),
+          axis.title.y.right = element_blank(),
+          axis.title.x = element_blank(),
+          plot.title = element_text(family = "Cambria",face = "bold", hjust = 0.5, size = 30),
+          panel.background = element_rect(fill = "white", colour = "white"),
+          panel.grid.major.y = element_line(color = "grey"))
+  
+}
 
